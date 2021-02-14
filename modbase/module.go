82837ef4bdd0a93d408bc618db0292cef0ad456d
Wariie <guilhem.mateo12@gmail.com>
2021-02-14 17:34:58 +0100

    Ping command - hub support & fix

--- modbase/module.go
+++ modbase/module.go
@@ -218,7 +218,7 @@ func (mod *ModuleImpl) serve() {
 		Handler: r,
 	}
 
-	go checkHubRunning(GetModManager().)
+	go checkHubRunning(&GetModManager().GetMod().HubServer)
 
 	if len(mod.Certs) == 2 { //CERTIFCATE AND KEY DETECTED
 		var cfg tls.Config
