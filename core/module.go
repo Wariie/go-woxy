3539df04bc6f9ec133f8bafdacddf756e32a774b
Wariie <guilhem.mateo12@gmail.com>
2021-02-15 21:18:18 +0100

    New test reverse

--- core/module.go
+++ core/module.go
@@ -234,25 +234,21 @@ func BasicAuth(a *auth.BasicAuth) gin.HandlerFunc {
 	}
 }
 
-// NewSingleHostReverseProxy -
-func NewSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
-	targetQuery := target.RawQuery
-	director := func(req *http.Request) {
-		req.URL.Scheme = target.Scheme
-		req.URL.Host = target.Host
-		if targetQuery == "" || req.URL.RawQuery == "" {
-			req.URL.RawQuery = targetQuery + req.URL.RawQuery
-		} else {
-			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-		}
-		if _, ok := req.Header["User-Agent"]; !ok {
-			// explicitly disable User-Agent so it's not set to default value
-			req.Header.Set("User-Agent", "")
-		}
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+
+		return a + b[1:]
+	case !aslash && !bslash:
+
+		return a + "/" + b
 	}
-	return &httputil.ReverseProxy{Director: director}
+	return a + b
 }
 
+// NewReverseProxy -
 func NewReverseProxy(target *url.URL) *httputil.ReverseProxy {
 	targetQuery := target.RawQuery
 	director := func(req *http.Request) {
@@ -275,7 +271,7 @@ func NewReverseProxy(target *url.URL) *httputil.ReverseProxy {
 		}
 	}
 
-	return &ReverseProxy{Director: director}
+	return &httputil.ReverseProxy{Director: director}
 }
 
 //ReverseProxy - reverse proxy for mod
@@ -296,7 +292,7 @@ func ReverseProxy(modName string, r Route) gin.HandlerFunc {
 				}
 				//TODO ADD CUSTOM HEADERS HERE
 
-				proxy := httputil.NewSingleHostReverseProxy(urlProxy)
+				proxy := NewReverseProxy(urlProxy)
 				proxy.ServeHTTP(c.Writer, c.Request)
 			}
 		} else {
